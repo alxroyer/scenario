2adf2c0dba7925e1ea49a8070e49ab6db21b2a08
Alexis ROYER <alexis.royer@gmail.com>
2024-09-26 19:58:31 +0200

    Adjust `scenario.tools.sphinx.simplifyreferences()` for `scenario.ui` (#83)
    Implementation details:
    - `short_ref` parameter split in `short_ref_package` and `short_ref_name`.
    - Regex adjusted to identify `scenario.ui` subpackage.

--- tools/src/scenario/tools/sphinx/_references.py
+++ tools/src/scenario/tools/sphinx/_references.py
@@ -21,7 +21,8 @@ import re
 def simplifyreferences(
         docname,  # type: str
         element,  # type: docutils.nodes.Element
-        short_ref=None,  # type: str
+        short_ref_package=None,  # type: str
+        short_ref_name=None,  # type: str
 ):  # type: (...) -> None
     """
     Recursive function applied in the end on final doc trees.
@@ -30,16 +31,18 @@ def simplifyreferences(
 
     _logger = Logger.getinstance(Logger.Id.SIMPLIFY_REFERENCES)  # type: Logger
 
-    _logger.debug("simplifyreferences(docname=%r, element=%r, short_ref=%r): element.attributes=%r",
-                  docname, element, short_ref, element.attributes)
+    _logger.debug("simplifyreferences(docname=%r, element=%r, short_ref_package=%r, short_ref_name): element.attributes=%r",
+                  docname, element, short_ref_package, short_ref_name, element.attributes)
 
     # :class:`docutils.nodes.reference` node: determine the short reference when applicable.
     if isinstance(element, docutils.nodes.reference):
         _reference = element  # type: docutils.nodes.reference
         _reftitle = _reference.get("reftitle", "")  # type: str
-        _match = re.match(r"^scenario\.([_a-z0-9]+)\.(.*)", _reftitle)
+        _match = re.match(r"^(scenario\.)(ui\.|)([_a-z0-9]+)\.(.*)", _reftitle)
         if _match:
-            short_ref = _match.group(2)
+            if _match.group(2):
+                short_ref_package = _match.group(1) + _match.group(2)
+            short_ref_name = _match.group(4)
         else:
             _logger.debug("'reftitle' %r does not match pattern", _reftitle)
 
@@ -47,20 +50,26 @@ def simplifyreferences(
         _child = element.children[_child_index]  # type: docutils.nodes.Node
         if isinstance(_child, docutils.nodes.Text):
             # Text children: simplify the text when `short_ref` is set.
-            if short_ref is not None:
-                _short_ref = short_ref  # type: str
+            if short_ref_name is not None:
+                _short_ref_name = short_ref_name  # type: str
                 if _child.endswith("()"):
-                    _short_ref += "()"
-                if _short_ref.endswith(_child):
-                    _logger.debug("Text %r is even shorter than %r, don't change it", _child, short_ref)
-                elif _child.endswith(_short_ref):
+                    _short_ref_name += "()"
+
+                if _short_ref_name.endswith(_child):
+                    _logger.debug("Text %r is even shorter than %r, don't change it", _child, _short_ref_name)
+                elif _child.endswith(_short_ref_name):
+                    _short_ref = (short_ref_package or "") + _short_ref_name  # type: str
                     _logger.debug("Simplifying %r >> %r", _child, _short_ref)
                     element.children[_child_index] = docutils.nodes.Text(_short_ref)
                 else:
-                    _logger.warning(f"{docname}: Mismatching text {_child!r} with expected short reference {short_ref!r}")
+                    _logger.warning(f"{docname}: Mismatching text {_child!r} with expected short reference {short_ref_name!r}")
         elif isinstance(_child, docutils.nodes.Element):
             # Element children: make recursive calls.
             with _logger.scenario_logger.pushindentation("  "):
-                simplifyreferences(docname, _child, short_ref=short_ref)
+                simplifyreferences(
+                    docname, _child,
+                    short_ref_package=short_ref_package,
+                    short_ref_name=short_ref_name,
+                )
         else:
             _logger.warning(f"{docname}: Unexpected kind of node {_child!r}")
